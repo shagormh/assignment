c1c0c910c89351d3768ee8b3c55e970b3a8c8b84
MD.Mehedi Hasan Shagor <134819307+shagormh@users.noreply.github.com>
2024-11-15 10:23:53 +0600

    c

--- lib/assignment2.dart
+++ lib/assignment2.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      home: profileScreen(),
+
+    );
+  }
+}
+
 class profileScreen extends StatelessWidget{
+  const profileScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('My Profile'),
-        backgroundColor: Color(0xffffc107),
+        title: const Text('My Profile'),
+        backgroundColor: const Color(0xffffc107),
         actions: [
           IconButton(onPressed: (){},
-          icon: Icon(Icons.add),
+          icon: const Icon(Icons.add),
           ),
           IconButton(onPressed: (){},
-            icon: Icon(Icons.settings),
+            icon: const Icon(Icons.settings),
           ),
           IconButton(onPressed: (){},
-            icon: Icon(Icons.call),
+            icon: const Icon(Icons.call),
           )
         ],
       ),
       body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
+        padding: const EdgeInsets.all(16.0),
+        children: const [
           CustomItem(icon: Icons.icecream_outlined, text: "Ice cream is very delicious right?"),
-          SizedBox(height: 20,),
+          SizedBox(height: 40,),
           CustomItem(icon: Icons.code_outlined, text: 'Programming is not boring if you love it'),
-          SizedBox(height: 20,),
+          SizedBox(height: 40,),
           CustomItem(icon: Icons.egg_outlined, text: 'If you submit code directly copy from chatgpt then mark will 0'),
 
         ],
@@ -43,10 +64,10 @@ class CustomItem extends StatelessWidget{
   final String text;
 
   const CustomItem({
-    Key? key,
+    super.key,
     required this.icon,
     required this.text,
-}):super(key: key);
+});
 
   @override
   Widget build(BuildContext context) {
@@ -56,19 +77,19 @@ class CustomItem extends StatelessWidget{
          Center(
            child: CircleAvatar(
              radius: 80,
-             backgroundColor: Color(0xffeaddff),
+             backgroundColor: const Color(0xffeaddff),
              child: Icon(
                icon,
-               color: Color(0xff21005d),
+               color: const Color(0xff21005d),
                size: 90,
 
              ),
            ),
          ),
-         SizedBox(height: 10,),
+         const SizedBox(height: 10,),
          Text(
            text,
-           style: TextStyle(
+           style: const TextStyle(
              color: Colors.black,
              fontSize: 16,
              fontWeight: FontWeight.bold,
